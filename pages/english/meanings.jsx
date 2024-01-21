0b2eba2dc76d7e0be209c613f78be40a0702c45d
LazizbekDev <lazizbektojiboyev0611@gmail.com>
2024-01-21 22:46:16 +0500

    english meanings.jsx

---
+++ pages/english/meanings.jsx
@@ -0,0 +1,257 @@
+import React from 'react';
+
+const Meanings = () => {
+    return (
+        <div style={{fontFamily: "sans-serif"}}>
+            <h2>By <a target={"_blank_"} href={"https://t.me/melod_1st"}>Zubair🌙</a></h2>
+            <p>AFA &ndash; All-Flash Array</p>
+            <p>BIOS &ndash; Basic Input Output System</p>
+            <p>BYTE &ndash; Storage Unit</p>
+            <p>CPU &ndash; Central Processing Unit</p>
+            <p>HDD &ndash; Hard Disk Drive</p>
+            <p>LCD &ndash; Liquid Crystal Display</p>
+            <p>OS &ndash; Operating System</p>
+            <p>PC &ndash; Personal Computer</p>
+            <p>PDF &ndash; Portable Document Format</p>
+            <p>RAID &ndash; Redundant Array of Independent Disks</p>
+            <p>RAM &ndash; Random Access Memory</p>
+            <p>RDMA &ndash; Remote Direct Memory Access</p>
+            <p>ROM &ndash; Read Only-Memory</p>
+            <p>SATA &ndash; Serial Advanced Technology Attachment&nbsp;</p>
+            <p>SSD &ndash; Solid State Drive</p>
+            <p>VGA &ndash; Video Graphics Array</p>
+            <p>2. List of Other Important Computer Abbreviations&nbsp;</p>
+            <p>COMPUTER &ndash; Commonly Operated Machine Used for Trade or Technology, Education and Research</p>
+            <p>AAC &ndash; Advanced Audio Coding</p>
+            <p>AI &ndash; Artificial Intelligence</p>
+            <p>ARPANET &ndash; Advanced Research Projects Agency Network</p>
+            <p>ALU &ndash; Arithmetic Logic Unit&nbsp;</p>
+            <p>ALGOL &ndash; Algorithmic Language&nbsp;</p>
+            <p>AOL &ndash; America Outline</p>
+            <p>API &ndash; Application Program Interface&nbsp;</p>
+            <p>APT &ndash; Automatically Programmed Tooling</p>
+            <p>ARP &ndash; Address Resolution Protocol</p>
+            <p>ASP &ndash; Active Server Pages</p>
+            <p>ATM &ndash; Asynchronous Transfer Mode</p>
+            <p>AUI &ndash; Attachment Unit Interface</p>
+            <p>AT &ndash; Advanced Technology</p>
+            <p>ASCII &ndash; American Standard Code for Information Interchange</p>
+            <p>AVI &ndash; Audio Video Interleave</p>
+            <p>BCC &ndash; Blind Carbon Copy</p>
+            <p>BASIC &ndash; Beginner&rsquo;s All-Purpose Symbolic Instruction Code</p>
+            <p>BPS &ndash; Bytes Per Second</p>
+            <p>BCD &ndash; Binary Coded Decimal</p>
+            <p>B HTML &ndash; Broadcast Hypertext Markup Language</p>
+            <p>BIU &ndash; Bus Interface Unit</p>
+            <p>BMP &ndash; Bitmap</p>
+            <p>B2C &ndash; Business to Consumer</p>
+            <p>B2B &ndash; Business to Business</p>
+            <p>CUI &ndash; Character User Interface</p>
+            <p>CSS &ndash; Cascading Style Sheets</p>
+            <p>CPI &ndash; Clock/Cycle Per Instruction</p>
+            <p>CC &ndash; Carbon Copy</p>
+            <p>CAI &ndash; Computer-Aided Instruction</p>
+            <p>CDMA &ndash; Code Division Multiple Access</p>
+            <p>CRT &ndash; Cathode Ray Tube</p>
+            <p>COBOL &ndash; Common Business Oriented Language&nbsp;</p>
+            <p>CISC &ndash; Complex Instructions Set Computers</p>
+            <p>CADD &ndash; Computer-Aided Design in Drafting</p>
+            <p>CAD &ndash; Computer-Aided Design</p>
+            <p>CD &ndash; Compact Disk</p>
+            <p>CD RW &ndash; Compact Disk Rewritable</p>
+            <p>CMD &ndash; Command</p>
+            <p>CD ROM &ndash; Compact Disk Read Only Memory</p>
+            <p>CROM &ndash; Computerised Range of Motion</p>
+            <p>CAM &ndash; Computer-Aided Manufacturing</p>
+            <p>DAT &ndash; Digital Audio Tape</p>
+            <p>DNA &ndash; Distributed Internet Architecture</p>
+            <p>DDR &ndash; Double Data Rate</p>
+            <p>DPI &ndash; Dots Per Inch</p>
+            <p>DDR-SDRAM &ndash; Double Data Rate-Synchronous Dynamic Random Access Memory</p>
+            <p>DML &ndash; Data Manipulation Language</p>
+            <p>DOS &ndash; Disk Operating System</p>
+            <p>DOC &ndash; Date Optimising Computer</p>
+            <p>DHTML &ndash; Dynamics Hypertext Markup Language</p>
+            <p>Doc &ndash; Document</p>
+            <p>DVD &ndash; Digital Versatile Disk</p>
+            <p>DVI -Digital Visual Interface</p>
+            <p>DDL &ndash; Data Definition Language</p>
+            <p>DRAM &ndash; Dynamic Random Access Memory</p>
+            <p>DBMS &ndash; Database Management System</p>
+            <p>DVDR &ndash; Digital Versatile Disk Recordable</p>
+            <p>DVDRW &ndash; Digital Versatile Disk Rewritable</p>
+            <p>DVR &ndash; Digital Video Recorder</p>
+            <p>DARPANET &ndash; Defence Advanced Research Project Agency Network</p>
+            <p>E-Commerce &ndash; Electronic Commerce</p>
+            <p>EDGE &ndash; Enhanced Data Rate for GSM Evolution</p>
+            <p>EDI &ndash; Electronic Data Interchange</p>
+            <p>EDP &ndash; Electronic Data Processing</p>
+            <p>FS &ndash; File System</p>
+            <p>FTP &ndash; File Transfer Protocol</p>
+            <p>FORTRAN &ndash; Formula Translation</p>
+            <p>FDD &ndash; Floppy Disk Drive</p>
+            <p>FDC &ndash; Floppy Disk Controller</p>
+            <p>ENIAC &ndash; Electronic Numerical Integrator and Calculator</p>
+            <p>EDSAC &ndash; Electronic Delay Storage Automatic Calculator</p>
+            <p>EDVAC &ndash; Electronic Discrete Variable Automatic Compute</p>
+            <p>EB &ndash; EXAByte</p>
+            <p>EiB &ndash; EXBIByte</p>
+            <p>EROM &ndash; Erasable Read-Only Memory&nbsp;</p>
+            <p>EPROM &ndash; Erasable Programmable Read-Only Memory</p>
+            <p>EEPROM &ndash; Electrically Erasable Programmable Read-Only Memory</p>
+            <p>E-Mail &ndash; Electronic Mail</p>
+            <p>EFS &ndash; Encrypted File System</p>
+            <p>FAT &ndash; Find Allocation Table</p>
+            <p>FPS -Frames Per Second&nbsp;</p>
+            <p>FLOPS &ndash; Floating Point Operations for Second</p>
+            <p>FM &ndash; Frequency Modulation</p>
+            <p>GB &ndash; GigaByte</p>
+            <p>GiB &ndash; GIBIByte</p>
+            <p>GIGO &ndash; Garbage in Garbage Out</p>
+            <p>GHz &ndash; Gigahertz</p>
+            <p>GIF &ndash; Graphics Interchangeable Format</p>
+            <p>GDI &ndash; Graphical Device Into</p>
+            <p>GPRS &ndash; General Packet Radio Service</p>
+            <p>GUI &ndash; Graphical User Interface</p>
+            <p>GBPS &ndash; Gigabytes/Gigabits Per Second</p>
+            <p>3GP &ndash; 3rd Generation Project</p>
+            <p>3GPP &ndash; 3rd Generation Partnership Project</p>
+            <p>GML &ndash; Geography Markup Language</p>
+            <p>GSM &ndash; Global System For Mobile Communication</p>
+            <p>IBM &ndash; International Business Machines</p>
+            <p>IC &ndash; Integrated Circuit</p>
+            <p>IO &ndash; Input Output</p>
+            <p>IOP &ndash; Input Output Processor</p>
+            <p>IPV6 &ndash; Internet Protocol Version 6</p>
+            <p>IPV4 &ndash; Internet Protocol Version 4</p>
+            <p>IP &ndash; Internet Protocol</p>
+            <p>INFO &ndash; Information</p>
+            <p>HDMI &ndash; High Definition Multimedia Interface&nbsp;</p>
+            <p>HTTPS &ndash; Hypertext Transfer Protocol Secure</p>
+            <p>HD &ndash; Hard Disk</p>
+            <p>HDD &ndash; Hard Disk Drive</p>
+            <p><br/></p>
+            <p>Zubair🌙, [1/20/2024 10:07 AM]</p>
+            <p>HPC &ndash; Handheld Personal Computer</p>
+            <p>HP &ndash; Hewlett Packard/Horsepower</p>
+            <p>INTEL &ndash; Integrated Electronics</p>
+            <p>IMAP &ndash; Internet Message Access Protocol</p>
+            <p>ISO &ndash; International Organisation for Standardisation</p>
+            <p>HSDPA &ndash; High-speed Downlink Packet Access</p>
+            <p>ICT &ndash; Information Communication Technology</p>
+            <p>IT &ndash; Information Technology</p>
+            <p>MIPS &ndash; Million Instructions for Second</p>
+            <p>MHz &ndash; Megahertz</p>
+            <p>MBPS &ndash; Megabits/Megabytes per Second</p>
+            <p>MB &ndash; Motherboard/Megabyte</p>
+            <p>MAN &ndash; Metropolitan Area Network&nbsp;</p>
+            <p>LLL &ndash; Low-level Language</p>
+            <p>Kbps &ndash; Kilobits/Kilobytes per Second</p>
+            <p>KBD &ndash; Keyboard</p>
+            <p>KB &ndash; Kilobyte</p>
+            <p>JSP &ndash; Java Server Page&nbsp;</p>
+            <p>JS &ndash; JavaScript</p>
+            <p>JPEG &ndash; Joint Photographic Expert Group&nbsp;</p>
+            <p>JAD &ndash; Job Application Descriptor/Development</p>
+            <p>J2EE &ndash; Java 2 Platform Enterprise Edition</p>
+            <p>JAR &ndash; Java Archive</p>
+            <p>MIME &ndash; Multipurpose Internet Mail Extensions</p>
+            <p>MICR &ndash; Magnetic Ink Character Recognition</p>
+            <p>&nbsp;MPEG &ndash; Motion Picture Experts Group</p>
+            <p>Mp3 &ndash; MPEG Audio Layer 3</p>
+            <p>Mp4 &ndash; MPEG-4 AVC</p>
+            <p>NAT &ndash; Network Address Translation</p>
+            <p>NIC &ndash; Network Interface Card</p>
+            <p>NIIT &ndash; National Institute of Information Technology</p>
+            <p>NTP &ndash; Network Time Protocol</p>
+            <p>NTFS &ndash; New Technology File System</p>
+            <p>OMR &ndash; Optical Mark Reader/Recognition</p>
+            <p>OOP &ndash; Object-Oriented Programming</p>
+            <p>OpenGL &ndash; Open Graphics Library</p>
+            <p>OSI &ndash; Open Systems Interconnection</p>
+            <p>PHP &ndash; Hypertext Preprocessor</p>
+            <p>PB &ndash; PetaByte</p>
+            <p>PiB &ndash; PebiByte</p>
+            <p>PNG &ndash; Portable Network</p>
+            <p>PNP &ndash; Plug and Play</p>
+            <p>PDA &ndash; Personal Digital</p>
+            <p>PDU &ndash; Protocol Data Unit/Power Distribution Unit</p>
+            <p>PAN &ndash; Personal Area Network</p>
+            <p>PROM &ndash; Programmable Read-Only Memory</p>
+            <p>PCI &ndash; Peripheral Component Interconnect</p>
+            <p>POST &ndash; Power on Self Test</p>
+            <p>PSU &ndash; Power Supply Unit</p>
+            <p>PING &ndash; Packet Internet/Internetwork Groper</p>
+            <p>RDBMS &ndash; Relational Database Management System</p>
+            <p>RIP &ndash; Routing Information to Protocol</p>
+            <p>RPM &ndash; Revolutions Per Minute</p>
+            <p>SMPS &ndash; Switch Mode Power Supply</p>
+            <p>SMTP &ndash; Simple Mail Transfer Protocol</p>
+            <p>SRAM &ndash; Static Random Access Memory</p>
+            <p>SIM &ndash; Subscriber Identity Module</p>
+            <p>SAM &ndash; Software Asset Management/Sequential Access Method</p>
+            <p>SNAP &ndash; Subnetwork Access Protocol</p>
+            <p>SNOBOL &ndash; String Oriented Symbolic Language</p>
+            <p>SIU &ndash; Serial Interface Unit</p>
+            <p>SW &ndash; Software</p>
+            <p>SMS &ndash; Short Message Services</p>
+            <p>SQL &ndash; Structured Query</p>
+            <p>TB &ndash; Terabyte</p>
+            <p>TiB &ndash; Tebibyte</p>
+            <p>TCP &ndash; Transmission Control Protocol&nbsp;</p>
+            <p>TBPS &ndash; Terabytes Per Second&nbsp;</p>
+            <p>TAPI &ndash; Telephony Application Programming Interface</p>
+            <p>TXT &ndash; Text</p>
+            <p>UNIVAC &ndash; Universal Automatic Computer</p>
+            <p>UHF &ndash; Ultra High Frequency</p>
+            <p>UMTS &ndash; Universal Mobile Telecommunication System</p>
+            <p>VAN &ndash; Value Added Network&nbsp;</p>
+            <p>UPS &ndash; Uninterruptible Power Supply</p>
+            <p>UI &ndash; User Interface</p>
+            <p>VDU &ndash; Visual Display Unit</p>
+            <p>VIRUS &ndash; Vital Information Resource Under Seized&nbsp;</p>
+            <p>VCD &ndash; Video Compact Disk</p>
+            <p>VHF &ndash; Very High Frequency</p>
+            <p>VOIP &ndash; VoiceOver Internet Protocol</p>
+            <p>VRAM &ndash; Video Random Access Memory</p>
+            <p>VPN &ndash; Virtual Private Network</p>
+            <p>WAN &ndash; Wide Area Network</p>
+            <p>WAP &ndash; Wireless Application Protocol</p>
+            <p>WORM &ndash; Write Once Read Many</p>
+            <p>WWW &ndash; World Wide Web</p>
+            <p>WBMP &ndash; Wireless Bitmap Image</p>
+            <p>WLAN &ndash; Wireless Local Area Network</p>
+            <p>WMV &ndash; Windows Media Video</p>
+            <p>WML &ndash; Wireless Markup Language</p>
+            <p>WMA &ndash; Windows Media Audio</p>
+            <p>WINS &ndash; Windows Internet Name Service&nbsp;</p>
+            <p>XT &ndash; Extended Technology</p>
+            <p>XMF &ndash; Extensible Music File</p>
+            <p>XML &ndash; Extensible Markup Language</p>
+            <p>XMS &ndash; Extended Memory Specification</p>
+            <p>XHTML &ndash; Extensible Hypertext Markup Language&nbsp;</p>
+            <p>XSL &ndash; Extensible Style Language</p>
+            <p>Yahoo &ndash; Yet Another Hierarchical Officious Oracle</p>
+            <p>YB &ndash; Yottabyte</p>
+            <p>YiB &ndash; Yobibyte</p>
+            <p>ZiB &ndash; ZebiByte&nbsp;</p>
+            <p>ZB &ndash; Zetta Byte</p>
+            <p>3. Computer Short Forms Related to Internet and Network</p>
+            <p>DNS &ndash; Domain Name Server&nbsp;</p>
+            <p>FTP &ndash; File Transport Protocol</p>
+            <p>HTML &ndash; HyperText Markup Language</p>
+            <p>HTTP &ndash; Hypertext Transfer Protocol</p>
+            <p>IP &ndash; Internet Protocol</p>
+            <p>ISP &ndash; Internet Service Provider</p>
+            <p>LAN &ndash; Local Area Network</p>
+            <p>PPP &ndash; Point to Point Protocol</p>
+            <p>SEO &ndash; Search Engine Optimisation&nbsp;</p>
+            <p>URL -Uniform Resource Locator&nbsp;</p>
+            <p>USB &ndash; Universal Serial Bus</p>
+            <p>VR &ndash; Virtual Reality</p>
+            <p>VRML &ndash; Virtual Reality Mark-up Language</p>
+        </div>
+    );
+};
+
+export default Meanings;
